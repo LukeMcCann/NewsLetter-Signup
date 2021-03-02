82e578f0ecf07f6d743ec1335fe789f21e2d2a80
LukeMcCann <developer-lukemccann@outlook.com>
2021-03-02 22:28:51 +0000

    Add bootstrap styling for basic sign in page

--- app.js
+++ app.js
@@ -3,9 +3,14 @@
 require('dotenv').config();
 const request = require('request');
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
-const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/signup.html');
+});
 
 app.listen(3000, function () {
     console.log('Listening on port: 3000');
